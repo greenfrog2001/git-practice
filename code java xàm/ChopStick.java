f8d43a1d2eff0d9b9f34e64a6d7191a0dc54651f
Uyen Khanh <89779147+uynkhanh1505@users.noreply.github.com>
2023-02-25 16:02:33 +0700

    Update ChopStick.java

--- code java xàm/ChopStick.java
+++ code java xàm/ChopStick.java
@@ -1,13 +1,6 @@
-/*
-	* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-	* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template 4	
-	* */
+
  package chuong10; 
- /**
-	*
-	* @author bajoe 
-	* 
-	*/
+
  public class ChopStick { 
 	boolean available; 
 	ChopStick() {
