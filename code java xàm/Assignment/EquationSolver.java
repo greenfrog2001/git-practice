810ddb257740882de0721d239128fe11e728e450
greenfrog2001 <phanviettan1@gmail.com>
2023-02-26 11:16:25 +0700

    init EquationSolver.java
    Sketch code

---
+++ code java xàm/Assignment/EquationSolver.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class EquationSolver{
+    public static void main(String args[]){
+        double a, b, c;
+
+        Scanner params = new Scanner(Scanner.in)
+        System.out.println("Enter a:");
+        double a = dd.nextInt();
+        System.out.println("Enter b:");
+        double b = dd.nextInt();
+        System.out.println("Enter c:");
+        double c = dd.nextInt();
+
+        if (a == 0) {
+            System.out.println("This equation has only one root x = ");
+        }
+        }
+
+    public EquationSolver() {
+    }
+}
