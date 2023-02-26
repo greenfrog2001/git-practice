4295de5de41736cda615b217271e3143277c3e18
greenfrog2001 <phanviettan1@gmail.com>
2023-02-26 11:24:42 +0700

    Update solver
    Implement solver in main

--- code java xàm/Assignment/EquationSolver.java
+++ code java xàm/Assignment/EquationSolver.java
@@ -3,20 +3,41 @@ import java.lang.Math;
 
 public class EquationSolver{
     public static void main(String args[]){
-        double a, b, c;
 
-        Scanner params = new Scanner(Scanner.in)
+        Scanner params = new Scanner(System.in);
         System.out.println("Enter a:");
-        double a = dd.nextInt();
+        double a = params.nextInt();
         System.out.println("Enter b:");
-        double b = dd.nextInt();
+        double b = params.nextInt();
         System.out.println("Enter c:");
-        double c = dd.nextInt();
+        double c = params.nextInt();
 
         if (a == 0) {
             System.out.println("This equation has only one root x = ");
         }
+        else {
+            double d = b*b - 4*a*c;
+            if (d < 0) {
+                System.out.println("This equation has two complex roots: ");
+                double x1 = (-b - Math.sqrt(d)) / (2*a);
+                double x2 = (-b + Math.sqrt(d)) / (2*a);
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+            else if (d == 0) {
+                System.out.println("This equation has one double root: ");
+                double x = - b / (2*a);
+                System.out.println("x - " + x);
+            }
+            else {
+                System.out.println("This equation has two real roots: ");
+                double x1 = (-b - Math.sqrt(d)) / (2*a);
+                double x2 = (-b + Math.sqrt(d)) / (2*a);
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
         }
+    }
 
     public EquationSolver() {
     }
