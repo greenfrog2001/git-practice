8bf18ae650197684938ea5d18b9654cd528a3344
Tan Phan <53978309+greenfrog2001@users.noreply.github.com>
2023-02-27 16:17:15 +0700

    Implement FibonacciNumbers
    Implement recursive solution

--- code java xàm/Assignment/FibonacciNumbers_GF.java
+++ code java xàm/Assignment/FibonacciNumbers_GF.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class FibonacciNumbers_GF {
-    
+    public static int FibonacciNumbers (int n) {
+        if (n == 0) {
+            return 0;
+        }
+        else if (n == 1 || n == 2) {
+            return 1;
+        }
+        else {
+            return FibonacciNumbers(n-1) + FibonacciNumbers(n-2);
+        }
+    }
+    public static void main (String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter n: ");
+        int n = input.nextInt();
+        int result = FibonacciNumbers(n);
+    }
 }
